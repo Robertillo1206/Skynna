c5bbe9cde7395fecb4954960f4d4c27130ef8c0b
Skynna FC <robertoaperalta06@gmail.com>
2026-04-05 17:05:38 +0200

    Update jugadores.js

--- jugadores.js
+++ jugadores.js
@@ -1,49 +1,11 @@
-const track = document.getElementById('track');
-const btnPrev = document.getElementById('btnPrev');
-const btnNext = document.getElementById('btnNext');
+<div class="carrusel-wrapper">
+    <button class="nav-carrusel prev-slide" id="btnPrev">&#10094;</button>
+    
+    <div class="carrusel-track" id="track">
+        <div class="carrusel-item"><img src="daniel1.jpeg" onclick="verGrande(this.src)"></div>
+        <div class="carrusel-item"><img src="daniel2.jpeg" onclick="verGrande(this.src)"></div>
+        <div class="carrusel-item"><img src="daniel3.jpeg" onclick="verGrande(this.src)"></div>
+    </div>
 
-// Cálculo dinámico según el tamaño de la pantalla en ese momento
-const getStep = () => {
-    const item = track.querySelector('.carrusel-item');
-    return item.offsetWidth + 15; 
-};
-
-btnNext.onclick = () => track.scrollBy({ left: getStep(), behavior: 'smooth' });
-btnPrev.onclick = () => track.scrollBy({ left: -getStep(), behavior: 'smooth' });
-
-// SOPORTE TÁCTIL Y RATÓN
-let isDown = false, startX, scrollLeft;
-
-const start = (e) => {
-    isDown = true;
-    startX = (e.pageX || e.touches[0].pageX) - track.offsetLeft;
-    scrollLeft = track.scrollLeft;
-    track.style.scrollBehavior = 'auto';
-};
-
-const end = () => {
-    isDown = false;
-    track.style.scrollBehavior = 'smooth';
-};
-
-const move = (e) => {
-    if (!isDown) return;
-    const x = (e.pageX || e.touches[0].pageX) - track.offsetLeft;
-    const walk = (x - startX) * 1.5;
-    track.scrollLeft = scrollLeft - walk;
-};
-
-track.addEventListener('mousedown', start);
-track.addEventListener('mousemove', move);
-track.addEventListener('mouseup', end);
-track.addEventListener('mouseleave', end);
-
-track.addEventListener('touchstart', start, {passive: true});
-track.addEventListener('touchmove', move, {passive: true});
-track.addEventListener('touchend', end);
-
-function verGrande(src) {
-    const modal = document.getElementById('visorModal');
-    modal.style.display = 'flex';
-    document.getElementById('imgGrande').src = src;
-}
+    <button class="nav-carrusel next-slide" id="btnNext">&#10095;</button>
+</div>
